232ec8b5c1ccf7936d7230f2f65fd04b68968cf2
Gerard Rocher-Ros <gerardrocher@gmail.com>
2018-03-14 10:41:19 +0100

    Update nighttime_reg_multiple.R

--- nighttime_reg_multiple.R
+++ nighttime_reg_multiple.R
@@ -21,7 +21,7 @@ suppressPackageStartupMessages({
 night_fun <- function(site, reg_start, reg_end){
   
   #first step is to make a new file with the daily mean values. This used only to be able to select each day, and take the temperature for the k600 calcs
-  daily = site %>% group_by(Daily = format(solar.time, "%Y-%m-%d")) %>%
+  daily = site %>% group_by(Daily = format(local.time, "%Y-%m-%d")) %>%
     summarise_all(funs(mean=mean(., na.rm=T)))
   
   #initialising variables
@@ -30,13 +30,14 @@ night_fun <- function(site, reg_start, reg_end){
   daily$intercept.se <- NA
   daily$intercept <- NA
   daily$slope.se <- NA
+  daily$k600 <- NA
   
   #the main loop to go through each day/night
-  for(i in 2:length(daily$Daily)-1){
-    
+  for(i in 5:length(daily$Daily)-1){
+    #print(paste("day", i))
     #extract the day and the day after to isolate the night period
-    day1 <- as.Date(daily$solar.time_mean[i])
-    day2 <- as.Date(daily$solar.time_mean[i+1])
+    day1 <- as.Date(daily$local.time_mean[i])
+    day2 <- as.Date(daily$local.time_mean[i+1])
     hour1 <- '15:00:00'
     
     #and make a POSIX.ct object to be able to subset the file later
@@ -44,7 +45,7 @@ night_fun <- function(site, reg_start, reg_end){
     Date2 <- as.POSIXct( paste(day2 , hour1))
     
     #this is the data for each night
-    test<-subset(site, solar.time> Date1 & solar.time < Date2)
+    test<-subset(site, local.time> Date1 & local.time < Date2)
     
     
     #Smooth O2 data and calculate dC/dt
@@ -69,7 +70,7 @@ night_fun <- function(site, reg_start, reg_end){
         reg_per2 <-  as.POSIXct(as.POSIXct(paste(day1 , reg_end))+twindow[j])
         
         #for each window, subset the data to make the regression
-        nightr_d <- subset(test, solar.time> reg_per1 & solar.time < reg_per2)
+        nightr_d <- subset(test, local.time> reg_per1 & local.time < reg_per2)
         
         #make a linear regression and store all variables of interest
         fit1 <- lm( nightr_d$deltaO2~nightr_d$O2def)
@@ -91,63 +92,64 @@ night_fun <- function(site, reg_start, reg_end){
     #and save the relevant variables
     daily$slope[i] <- varyreg2$slope[bestie]
     daily$intercept[i] <- varyreg2$intercept[bestie]
-    daily$slope.se[i] <- varyreg$slope.se[bestie]
+    daily$slope.se[i] <- varyreg2$slope.se[bestie]
     daily$r2[i]    <- varyreg2$r2[bestie]
     #finally convert the slope to k600 with the temperature correction
-    daily$k600[i]<- ((600/(1800.6-(120.1*daily$temp.water_mean[i])+(3.7818*daily$temp.water_mean[i]^2)-(0.047608*daily$temp.water_mean[i]^3)))^-0.5)*varyreg$slope[bestie]
+    daily$k600[i]<- ((600/(1800.6-(120.1*daily$temp.water_mean[i])+(3.7818*daily$temp.water_mean[i]^2)-(0.047608*daily$temp.water_mean[i]^3)))^-0.5)*varyreg2$slope[bestie]
         
     ##### THIS IS A BONUS, TO MAKE A COMPOSITE PLOT OF [O2], dO2, LIGHT AND THE REGRESSION ####
     ####It is disconnected by default, if you want to use it make sure you have light data also
-          # 
-          # #recover the best regression and make it again
-          # reg_per1 <- as.POSIXct(as.POSIXct(paste(day1 , reg_start))+twindow[bestie])
-          # reg_per2 <-  as.POSIXct(as.POSIXct(paste(day1 , reg_end))+twindow[bestie])
-          # nightr_d <- subset(test, solar.time> reg_per1 & solar.time < reg_per2)
-          # 
-          # #make a plot of the regression
-          # night_regfit<- ggplot(data= nightr_d, aes(x=O2def, y=deltaO2))+
-          #   geom_point()+
-          #   geom_smooth(method=lm)+
-          #   geom_text(x = quantile(nightr_d$O2def, 0.5, na.rm=T), y = quantile(nightr_d$deltaO2, 0.95, na.rm=T), label =lm_eqn(nightr_d$O2def,nightr_d$deltaO2), parse = TRUE)+
-          #   labs(x= "O2 def(mgO2 L-1)",y="dO2 (mgO2 L-1 d-1)")+
-          #   theme(axis.text = element_text(size=14), axis.title = element_text(size=14))
-          # 
-          # # a plot for the O2 deficit
-          # o2def <-ggplot(data=test)+
-          #   geom_line(aes(x=as.POSIXct(solar.time), y=DO.sat-DO.obs), size=2, color="blue3")+
-          #   scale_x_datetime(date_breaks = "6 hour", labels=date_format("%d-%H"))+
-          #   geom_vline(xintercept = as.numeric(reg_per1), linetype=4)+
-          #   geom_vline(xintercept = as.numeric(reg_per2), linetype=4)+
-          #   labs(x="day-hour" ,y="O2 def(mgO2 L-1)")+
-          #   theme(axis.text = element_text(size=14), axis.title = element_text(size=14)) +theme_bw()
-          # 
-          #  #and the delta O2
-          # o2_delta <- ggplot(data=test)+
-          #   geom_line(aes(x=as.POSIXct(solar.time), y=deltaO2), size=2, color="darkred")+
-          #   scale_x_datetime(date_breaks = "6 hour", labels=date_format("%d-%H"))+
-          #   geom_vline(xintercept = as.numeric(reg_per1), linetype=4)+
-          #   geom_vline(xintercept = as.numeric(reg_per2), linetype=4)+
-          #   labs(x="day-hour" ,y="dO2 (mgO2 L-1 d-1)")+
-          #   theme(axis.text = element_text(size=14), axis.title = element_text(size=14)) +theme_bw()
-          # #the light
-          # lightp <- ggplot(data=test)+
-          #   geom_line(aes(x=as.POSIXct(solar.time), y=light), size=2, color="goldenrod2")+
-          #   scale_x_datetime(date_breaks = "6 hour", labels=date_format("%d-%H"))+
-          #   labs(x="day-hour" ,y="light")+
-          #   theme(axis.text = element_text(size=14), axis.title = element_text(size=14)) +theme_bw()
-          # 
-          # #make a pannel with them
-          # pan1<-  ggarrange(o2def, o2_delta, lightp, night_regfit + rremove("x.text"), ncol = 2, nrow = 2)
-          # 
-          # name <- deparse(substitute(site))
-          # 
-          # #and export it with the name of the site and the date as title
-          # pan1 %>%
-          #   ggexport(filename = paste(name,'_', day1,'.png', sep=""),res = 170, width = 1000, height = 1000)
-       #####   
+# 
+#        #recover the best regression and make it again
+#        reg_per1 <- as.POSIXct(as.POSIXct(paste(day1 , reg_start))+twindow[bestie])
+#        reg_per2 <-  as.POSIXct(as.POSIXct(paste(day1 , reg_end))+twindow[bestie])
+#        nightr_d <- subset(test, local.time> reg_per1 & local.time < reg_per2)
+# 
+#        #make a plot of the regression
+#        night_regfit<- ggplot(data= nightr_d, aes(x=O2def, y=deltaO2))+
+#          geom_point()+
+#          geom_smooth(method=lm)+
+#          geom_text(x = quantile(nightr_d$O2def, 0.5, na.rm=T), y = quantile(nightr_d$deltaO2, 0.95, na.rm=T), label =lm_eqn(nightr_d$O2def,nightr_d$deltaO2), parse = TRUE)+
+#          labs(x= "O2 def(mgO2 L-1)",y="dO2 (mgO2 L-1 d-1)")+
+#          theme(axis.text = element_text(size=14), axis.title = element_text(size=14))
+# 
+#        # a plot for the O2 deficit
+#        o2def <-ggplot(data=test)+
+#          geom_line(aes(x=as.POSIXct(local.time), y=DO.sat-DO.obs), size=2, color="blue3")+
+#          scale_x_datetime(date_breaks = "6 hour", labels=date_format("%d-%H"))+
+#          geom_vline(xintercept = as.numeric(reg_per1), linetype=4)+
+#          geom_vline(xintercept = as.numeric(reg_per2), linetype=4)+
+#          labs(x="day-hour" ,y="O2 def(mgO2 L-1)")+
+#          theme(axis.text = element_text(size=14), axis.title = element_text(size=14)) +theme_bw()
+# 
+#         #and the delta O2
+#        o2_delta <- ggplot(data=test)+
+#          geom_line(aes(x=as.POSIXct(local.time), y=deltaO2), size=2, color="darkred")+
+#          scale_x_datetime(date_breaks = "6 hour", labels=date_format("%d-%H"))+
+#          geom_vline(xintercept = as.numeric(reg_per1), linetype=4)+
+#          geom_vline(xintercept = as.numeric(reg_per2), linetype=4)+
+#          labs(x="day-hour" ,y="dO2 (mgO2 L-1 d-1)")+
+#          theme(axis.text = element_text(size=14), axis.title = element_text(size=14)) +theme_bw()
+#        #the light
+#        lightp <- ggplot(data=test)+
+#          geom_line(aes(x=as.POSIXct(local.time), y=light), size=2, color="goldenrod2")+
+#          scale_x_datetime(date_breaks = "6 hour", labels=date_format("%d-%H"))+
+#          labs(x="day-hour" ,y="light")+
+#          theme(axis.text = element_text(size=14), axis.title = element_text(size=14)) +theme_bw()
+# 
+#        #make a pannel with them
+#        pan1<-  ggarrange(o2def, o2_delta, lightp, night_regfit + rremove("x.text"), ncol = 2, nrow = 2)
+# 
+#        name <- deparse(substitute(site))
+# 
+#        #and export it with the name of the site and the date as title
+#        pan1 %>%
+#          ggexport(filename = paste(name,'_', day1,'.png', sep=""),res = 170, width = 1000, height = 1000)
+       ####
           
     }
-    
+  
+  
   #and return the daily file
   daily
 }
